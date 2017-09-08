d8df38e7252844bde06b8e0b3fe9b047ea1d1ce0
malvarado <malvarado@USILG-DVM0062.wrbts.ads.wrberkley.com>
2017-09-08 13:24:09 -0500

    Adding Jasypt to encrypt the password connection to the database.

--- src/main/java/com/pei/billinginquiry/Application.java
+++ src/main/java/com/pei/billinginquiry/Application.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application
 {
     public static void main(String[] args) {
+        System.setProperty("jasypt.encryptor.password", "H4rD-70-6u35S");
         SpringApplication.run(Application.class, args);
     }
 }
